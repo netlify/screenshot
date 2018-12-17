3893c60384a3b6a7b20334940ef301c617962815
Ricardo Bartolome <ricardo@netlify.com>
2018-12-17 16:20:04 +0100

    Jenkins pipeline for generating k8s manifests

--- kubernetes/build-k8s.sh
+++ kubernetes/build-k8s.sh
@@ -23,8 +23,8 @@ sys.stdout.write(
 set -xe
 set -o pipefail
 
-if [[ $BITBALLOON_GIT_REF == "" ]]; then
-  echo "\$BITBALLOON_GIT_REF not provided"
+if [[ $screenshot_GIT_REF == "" ]]; then
+  echo "\$screenshot_GIT_REF not provided"
   exit 1
 fi
 
@@ -50,7 +50,7 @@ fi
 
 # Prepare paths
 SPINNAKER_GCS_PATH="$GCS_BUCKET/spinnaker-k8s-artifacts"
-BB_GCS_PATH="$GCS_BUCKET/bitballoon-k8s-artifacts/$BITBALLOON_GIT_REF"
+BB_GCS_PATH="$GCS_BUCKET/screenshot-k8s-artifacts/$screenshot_GIT_REF"
 
 # Prepare GCS auth
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
@@ -63,8 +63,8 @@ IMAGE_DIGEST=$(gsutil cat $BB_GCS_PATH/image-digest)
 # Prepare K8S manifest
 gsutil cp -r $BB_GCS_PATH/templates/* .
 
-# i.e.: gs://netlify-infrastructure/spinnaker-k8s-artifacts/gke/origin-poc/production/bitballoon/master
-K8S_GS_PATH_BASE=$SPINNAKER_GCS_PATH/$K8S_PROVIDER/$K8S_CLUSTER/$K8S_ENV/bitballoon/$BITBALLOON_GIT_REF
+# i.e.: gs://netlify-infrastructure/spinnaker-k8s-artifacts/gke/origin-poc/production/screenshot/master
+K8S_GS_PATH_BASE=$SPINNAKER_GCS_PATH/$K8S_PROVIDER/$K8S_CLUSTER/$K8S_ENV/screenshot/$screenshot_GIT_REF
 
 # Get git info from the build being deployed
 export BUILD_GIT_COMMIT=$(gsutil cat $BB_GCS_PATH/git-commit)
@@ -72,23 +72,12 @@ export BUILD_GIT_BRANCH=$(gsutil cat $BB_GCS_PATH/git-branch)
 
 # Build manifests
 suffix=$GIT_COMMIT.$(date +%s%N) # Different jobs may build the same commit SHA. Make the filenames different using a timestamp
-for role in api web bg sidekiq; do
-  export K8S_ROLE=$role
-  jinja2_cmd <bitballoon.yml.j2 >deployment.$role.$suffix.yml
-
-  if [[ ($role == "api" || $role == "web") && ($K8S_ENV == "production" || $K8S_ENV == "staging") ]]; then
-    jinja2_cmd <balancer.yml.j2 >balancer.$role.$suffix.yml
-  fi
-done
+jinja2_cmd <screenshot.yml.j2 >deployment.$suffix.yml
+jinja2_cmd <balancer.yml.j2 >balancer.$suffix.yml
 
 # Upload manifests
-for role in api web bg sidekiq; do
-  gsutil cp deployment.$role.$suffix.yml $K8S_GS_PATH_BASE/
-
-  if [[ ($role == "api" || $role == "web") && ($K8S_ENV == "production" || $K8S_ENV == "staging") ]]; then
-    gsutil cp balancer.$role.$suffix.yml $K8S_GS_PATH_BASE/
-  fi
-done
+gsutil cp deployment.$suffix.yml $K8S_GS_PATH_BASE/
+gsutil cp balancer.$suffix.yml $K8S_GS_PATH_BASE/
 
 echo "Generate spinnaker artifacts"
 cat > spinnaker-artifacts.yml <<EOF
@@ -96,24 +85,12 @@ git_branch: $BUILD_GIT_BRANCH
 git_commit: $BUILD_GIT_COMMIT
 artifacts:
   - type: "docker/image"
-    name: "gcr.io/netlify-services/bitballoon"
+    name: "gcr.io/netlify-services/screenshot"
     reference: "$IMAGE_DIGEST"
   - type: gcs/object
-    name: "deployment.api.yml"
-    reference: "$K8S_GS_PATH_BASE/deployment.api.$suffix.yml"
+    name: "deployment.yml"
+    reference: "$K8S_GS_PATH_BASE/deployment.$suffix.yml"
   - type: gcs/object
-    name: "balancer.api.yml"
-    reference: "$K8S_GS_PATH_BASE/balancer.api.$suffix.yml"
-  - type: gcs/object
-    name: "deployment.web.yml"
-    reference: "$K8S_GS_PATH_BASE/deployment.web.$suffix.yml"
-  - type: gcs/object
-    name: "balancer.web.yml"
-    reference: "$K8S_GS_PATH_BASE/balancer.web.$suffix.yml"
-  - type: gcs/object
-    name: "deployment.bg.yml"
-    reference: "$K8S_GS_PATH_BASE/deployment.bg.$suffix.yml"
-  - type: gcs/object
-    name: "deployment.sidekiq.yml"
-    reference: "$K8S_GS_PATH_BASE/deployment.sidekiq.$suffix.yml"
+    name: "balancer.yml"
+    reference: "$K8S_GS_PATH_BASE/balancer.$suffix.yml"
 EOF
